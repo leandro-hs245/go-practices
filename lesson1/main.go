b731aa08728e9b7705542be0b651273f8a436686
Leandro Hurtado <leandro.hs245@gmail.com>
2026-04-21 22:36:21 -0500

    Enhance lesson 1 with iota demonstration and variable shadowing examples

--- lesson1/main.go
+++ lesson1/main.go
@@ -2,7 +2,35 @@ package main
 
 import "fmt"
 
+var name = "Golang"
+
 const version = "0.0.1"
+
+const (
+	monday = iota
+	tuesday
+	wednesday
+	thursday
+	friday
+	saturday
+	sunday
+)
+
+const (
+	kb = 1 << (10 * iota)
+	mb
+	gb
+	tb
+	pb
+	eb
+)
+const (
+	_          = iota
+	debugLevel = iota << 1
+	infoLevel
+	warningLevel
+	errorLevel
+)
 
 func main() {
 	/* Go is a statically typed language, which means that the type of a variable is determined at compile time.
@@ -13,8 +41,10 @@ func main() {
 	var number int = 1
 	fmt.Println(number)
 	cpp, python, golang := true, false, "Golang!"
-	fmt.Println("Languages: ", cpp, python, golang)
+	fmt.Println("Languages: ", cpp, python, golang, name)
 	zeroValue()
+	printIota()
+	shadowing()
 }
 
 func zeroValue() {
@@ -35,4 +65,30 @@ func zeroValue() {
 	fmt.Println("Zero values for byte: ", bt)
 	fmt.Println("Zero values for rune: ", r)
 	fmt.Println("Zero values for map: ", m)
+}
+
+func printIota() {
+	/*
+		`iota` is a predeclared identifier that represents successive untyped integer constants.
+		It is reset to 0 whenever the `const` keyword appears in the source code and
+		increments by 1 for each constant specification in the same block. It is often used to create enumerated constants.
+	*/
+	fmt.Println("Days of the week: ", monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+	fmt.Println("Data sizes: ", kb, mb, gb, tb, pb, eb)
+	fmt.Println("Log levels: ", debugLevel, infoLevel, warningLevel, errorLevel)
+}
+
+func shadowing() {
+	/*
+		Variable shadowing occurs when a variable declared within a certain scope (e.g., an inner block) has the same name as a variable declared in an outer scope.
+		The inner variable "shadows" the outer variable, meaning that within the inner scope,
+		references to that variable name will refer to the inner variable rather than the outer one.
+	*/
+	x := 10
+	fmt.Println("Outer x: ", x)
+	{
+		x := 11
+		fmt.Println("Inner x: ", x)
+	}
+	fmt.Println("Outer x after inner block: ", x)
 }
